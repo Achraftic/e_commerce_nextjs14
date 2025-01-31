22dfa57b9556fba1fc0f5badac5398f6425cf724
Achraftic <ticachraf@gmail.com>
2025-01-31 12:01:58 +0100

    fix bug

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,19 +1,15 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
-
 import Credentials from "next-auth/providers/credentials";
-import Google from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import bcrypt from "bcryptjs"
-
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import bcrypt from "bcryptjs";
 import prisma from "../prisma/db";
 import { LoginAuthSchema } from "./shema/shema";
-
-
 
 export const authConfig = {
   adapter: PrismaAdapter(prisma),
   pages: {
-    signIn: "/signin"
+    signIn: "/signin",
   },
   session: {
     maxAge: 60 * 60 * 24 * 30,
@@ -21,79 +17,69 @@ export const authConfig = {
   },
 
   providers: [
-    Google
-    ,
+    Google,
     Credentials({
       async authorize(credentials) {
-        const validateDataAuth = LoginAuthSchema.safeParse(credentials)
+        const validateDataAuth = LoginAuthSchema.safeParse(credentials);
         if (!validateDataAuth.success) {
           return null;
         }
         const { email, password } = validateDataAuth.data;
-      
+
         const user = await prisma.user.findUnique({
-          where: {
-            email
-          }
-        })
-        console.log(user)
+          where: { email },
+        });
+
         if (!user) {
-          console.log("email or password are incorect")
-          return null
-        }
-        const ismatch = await bcrypt.compare(password, user?.hashedPassword as string)
-        if (!ismatch) {
-          console.log("Email or password are incorect")
-          return null
+          console.log("Email or password is incorrect");
+          return null;
         }
 
-        return { email: user.email, id: user.id ,image:user?.image,role:user?.role,name:user?.name};
-      }
-    })
+        const isMatch = await bcrypt.compare(password, user.hashedPassword as string);
+        if (!isMatch) {
+          console.log("Email or password is incorrect");
+          return null;
+        }
 
+        return {
+          email: user.email,
+          id: user.id,
+          image: user?.image,
+          role: user?.role,
+          name: user?.name,
+        };
+      },
+    }),
   ],
   callbacks: {
     authorized: ({ auth, request }) => {
       const isLoggedIn = Boolean(auth?.user);
-      const securepath = request.nextUrl.pathname
-      if (!isLoggedIn && (securepath.includes("/dashboard"))) {
-        return false;
-      }
-      return true
+      const securePath = request.nextUrl.pathname;
+      return isLoggedIn || !securePath.includes("/dashboard");
     },
-    async redirect({ url, baseUrl, token }) {
+    async redirect({ url, baseUrl, token }: { url: string; baseUrl: string; token?: { user?: { role?: string } } }) {
       const callbackUrl = new URL(url).searchParams.get("callbackUrl");
-      const userRole = token?.user?.role; // Ensure role is included in token
-      if (userRole === "admin") {
-        // Admin can access all URLs
-        return callbackUrl || `${baseUrl}/dashboard`;
-      } else {
-        // Redirect non-admin users to a specific page
-        return callbackUrl || `${baseUrl}/`; // Change this to your unauthorized page
-      }
-
-
+      const userRole = token?.user?.role;
+      return userRole === "admin" ? callbackUrl || `${baseUrl}/dashboard` : callbackUrl || `${baseUrl}/`;
     },
     jwt: ({ token, user }) => {
-     
       if (user) {
-
-        token.userId = user.id
-        token.role = user.role; // Add role to the token
+        token = {
+          ...token,
+          userId: user.id,
+          role: user.role,
+        };
       }
-      return token
+      return token;
     },
     session: ({ session, token }) => {
+      if (session.user) {
+        session.user.id = token.userId as string;
+        session.user.role = token.role as string;
+      }
+      return session;
+    },
+  },
+} satisfies NextAuthConfig;
 
-      session.user.id = token.userId
-      session.user.role = token.role; 
-      return session
-    }
-
-
-
-
-  }
-} satisfies NextAuthConfig
-
-export const { auth, signIn, signOut, handlers } = NextAuth(authConfig)
+export const { auth, signIn, signOut, handlers } = NextAuth(authConfig);
